536eec42ed11725a9bdc0de74bfde5da92e1670a
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-23 17:18:01 +0200

    style: styled admin dashboard page with cards

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,33 @@
 @extends('layouts/admin')
 
 @section('content')
-  <h1>Administration page</h1>
+<h1>Administration page</h1>
 <hr>
 
-<ul>
-  <li><a href="{{route('admin.projects.index')}}">Mostra tutti i progetti</a></li>
-  <li><a href="{{route('admin.types.index')}}">Mostra tutte le tipologie</a></li>
-</ul>
+<div class="_container">
+  <a href="{{route('admin.projects.index')}}" class="card-section">
+      <div class="_card">
+          <h3>Progetti</h3>
+          <img src="{{Vite::asset('resources/img/projects.png')}}" alt="Projects">
+          <div class="text">
+              In questa sezione vengono riportati tutti i progetti presenti sul sito.
+              <br>
+              <br>
+              Clicca sulla card per la gestione.
+          </div>
+      </div>
+  </a>
+  <a href="{{route('admin.types.index')}}" class="card-section">
+      <div class="_card">
+          <h3>Tipologie</h3>
+          <img src="{{Vite::asset('resources/img/types.png')}}" alt="Types">
+          <div class="text">
+              In questa sezione vengono riportate tutte le tipologie dei progetti.
+              <br>
+              <br>
+              Clicca sulla card per la gestione.
+          </div>
+      </div>
+  </a>
+</div>
 @endsection
