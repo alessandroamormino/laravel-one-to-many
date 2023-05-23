165d13be1e539edd03d877a13befbfca97f460a5
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-23 14:10:28 +0200

    fix: moved logout bar in layout/admin

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,39 +2,6 @@
 
 @section('content')
 
-<ul class="navbar-nav ml-auto">
-  <!-- Authentication Links -->
-  @guest
-  <li class="nav-item">
-      <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-  </li>
-  @if (Route::has('register'))
-  <li class="nav-item">
-      <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-  </li>
-  @endif
-  @else
-  <li class="nav-item dropdown">
-      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-          {{ Auth::user()->name }}
-      </a>
-
-      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="{{route('admin.dashboard.home')}}">{{__('Dashboard')}}</a>
-          <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
-          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                               document.getElementById('logout-form').submit();">
-              {{ __('Logout') }}
-          </a>
-
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-              @csrf
-          </form>
-      </div>
-  </li>
-  @endguest
-</ul>
-
 <div class="main">
   <h1>{{$project->title}}</h1>
   <h6>Categoria: {{$project->type?->name}}</h6>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -31,7 +31,39 @@
     @include('partials/navbar')
     @include('partials/explorer')
     <main class="admin">
-        @yield('content')
+      <ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+        @guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @if (Route::has('register'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        </li>
+        @endif
+        @else
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+            </a>
+      
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{route('admin.dashboard.home')}}">{{__('Dashboard')}}</a>
+                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+      
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+        </li>
+        @endguest
+      </ul>
+      @yield('content')
     </main>
   </div>
 </body>
